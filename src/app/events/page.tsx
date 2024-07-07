1f69c324104e3b5f82a5d881dd1a03cb4d08d959
shashikant010 <shashikantyadav0503@gmail.com>
2024-07-07 12:23:14 +0530

    fixed some issues

---
+++ src/app/events/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useEffect } from 'react'
+import Image from 'next/image'
+import React from 'react'
+
+function Page() {
+  useEffect(() => {
+    document.title = "Events | Hapkido";
+  }, []);
+  return (
+    <div className='flex justify-center items-center flex-col'>
+        <h1 className='p-6 rounded-full bg-blue-900 text-white md:text-3xl text-xl text-center font-bold w-1/2 m-2'>Events</h1>
+        <div className='self-start'>
+        
+        <p className='p-3 m-2'>Stay tuned for Latest Events....</p></div>
+    </div>
+  )
+}
+
+export default Page
